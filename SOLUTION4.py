d1f6aaa7854266853031fe8fde981329414b0622
Mohamed Khodeir <mkhodeir@ngvc.local>
2023-11-15 15:27:34 +0200

    Dasd

---
+++ SOLUTION4.py
@@ -0,0 +1,21 @@
+class Cat:
+    def __init__(self, name, type, color, age):
+        self.name = name
+        self.type = type
+        self.color = color
+        self.age = age
+    
+    def run():
+        print("I'm running now weeeeeeeee ......")
+        
+    def walk():
+        print("should I walk now ....... wooooooow")
+        
+    def meow():
+        print("meow meow meow")
+        
+
+catOne = Cat("hanafy", )
+catOne = Cat("hanafy", )
+catOne = Cat("hanafy", )
+print("dasdadaddsafasasdasdasdasdasdasdaddasasasdasdasdd")
